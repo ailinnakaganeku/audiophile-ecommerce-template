8347487915790468f6482c46b8f7a9bb13733f58
ailin.nakaganeku <ailin.nakaganeku@zenvia.com>
2023-04-06 19:33:42 -0300

    adding breakpoints - mobile styles w tailwindcss

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -2,9 +2,11 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold text-gray-900 mb-4">404 Not Found</h1>
-      <p className="text-lg text-gray-700 mb-8">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl sm:text-4xl font-bold text-gray-900 mb-4 text-center px-4">
+        404 Not Found
+      </h1>
+      <p className="text-base sm:text-lg text-gray-700 mb-8 text-center px-4">
         Oops! Looks like the page you are trying to access does not exist.
       </p>
       <Link
