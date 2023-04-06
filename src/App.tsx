0b925c17031034444bf273d6957f50143993df28
ailin.nakaganeku <ailin.nakaganeku@zenvia.com>
2023-04-06 11:55:16 -0300

    adding loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import ProductPage from "./components/ProductPage";
 import Footer from "./components/Footer";
+import Loader from "./components/Loader";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+    return () => clearTimeout(timeoutId);
+  }, []);
+
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product/:slug" element={<ProductPage />} />
-      </Routes>
-      <Footer />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:slug" element={<ProductPage />} />
+          </Routes>
+          <Footer />
+        </>
+      )}
     </Router>
   );
 };
